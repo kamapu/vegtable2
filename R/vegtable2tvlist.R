e0684f50ffda14af923e6a541186b739e3724dae
Miguel Alvarez <malvarez@uni-bonn.de>
2016-04-06 11:49:29 +0200

    New function: vegtable2tvlist

---
+++ R/vegtable2tvlist.R
@@ -0,0 +1,51 @@
+# TODO:   Convert taxlist to turboveg-like list and protopype for export
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+# Inverse function to TCS.replace2 (see in tvsplist, package taxlist)
+TCS.replace2.back <- function(x) {
+    x <- replace(x, x == "TaxonUsageID", "SPECIES_NR")
+    x <- replace(x, x == "TaxonName", "ABBREVIAT")
+    x <- replace(x, x == "AuthorName", "AUTHOR")
+    x <- replace(x, x == "TaxonConceptID", "VALID_NR")
+}
+
+# Internal function to create LETTERCODE
+get_code <- function(x) {
+    x <- strsplit(x, " ", fixed=TRUE)
+    x <- sapply(x, "[", c(1,2,4))
+    x[2,!is.na(x[3,])] <- paste(substr(x[2,], 1, 1), substr(x[3,], 1, 1),
+            sep=".")[!is.na(x[3,])]
+    x[2,x[2,] %in% c("species","spec.",".sp")] <- ".SP"
+    x <- toupper(paste0(substr(x[1,], 1, 4), substr(x[2,], 1, 3)))
+    return(x)
+}
+
+# The function
+vegtable2tvlist <- function(taxlist, ecodbase=TRUE) {
+    if(class(taxlist) != "taxlist")
+        stop("'taxlist' have to be of class 'taxlist'", call.=FALSE)
+    if(ecodbase & ncol(taxlist@taxonTraits) > 1)
+        Traits <- taxlist@taxonTraits else Traits <- NULL
+    # Add synonyms
+    taxlist@taxonNames$SYNONYM <- taxlist@taxonNames$TaxonConceptID %in%
+            taxlist@taxonRelations$ValidName
+    # Change taxlist
+    taxlist <- taxlist@taxonNames
+    # Additional standard columns
+    taxlist$SHORTNAME <- substr(taxlist$TaxonName, 1, 22)
+    taxlist$NATIVENAME <- ""
+    taxlist$LETTERCODE <- get_code(taxlist$TaxonName)
+    # Final version
+    colnames(taxlist) <- TCS.replace2.back(colnames(taxlist))
+    Head <- c("SPECIES_NR","LETTERCODE","SHORTNAME","ABBREVIAT","NATIVENAME",
+            "AUTHOR","SYNONYM","VALID_NR")
+    Head <- c(Head, colnames(taxlist)[!colnames(taxlist) %in% Head])
+    taxlist <- list(species=taxlist[,Head])
+    if(!is.null(Traits)) {
+        colnames(Traits)[1] <- "SPECIES_NR"
+        taxlist[["ecodbase"]] <- Traits
+    }
+    return(taxlist)
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+
+
 # vegtables2
-Extended version for personal use
+
+This is a package with functions for the internal use and adapted to own
+purposes.
+The functions included here are extensions to `taxlist`
+([https://github.com/kamapu/taxlist]()) and `vegtables`
+([https://github.com/kamapu/vegtables]()) and its functions may not work with
+any database.
+
+## Updating to the last version of vegtables
+The very first step is to install the package
+[devtools](https://github.com/hadley/devtools) and dependencies.
+Then you just need to execute following commands in your R-session:
+
+
+```r
+library(devtools)
+install_github("kamapu/vegtables2")
+```
