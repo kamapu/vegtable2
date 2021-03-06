1248463521251a652918d69946424061c0c1e02d
Miguel Alvarez <kamapu78@gmail.com>
2021-03-06 19:41:48 +0100

    Tested function

--- R/pg_merge_concepts.R
+++ R/pg_merge_concepts.R
@@ -41,66 +41,33 @@ pg_merge_concepts.PostgreSQLConnection <- function(conn, names2concepts,
 	Query <- paste0("UPDATE \"", paste0(names2concepts, collapse = "\".\""),
 			"\"\n",
 			"SET \"NameStatus\" = 'synonym'\n",
-			"WHERE \"TaxonConceptID\" in (",
+			"WHERE \"TaxonConceptID\" IN (",
 			paste(concept_id[-1], collapse=","),");\n")
 	dbSendQuery(conn, Query)
 	# change names to concept
 	Query <- paste0("UPDATE \"", paste0(names2concepts, collapse = "\".\""),
 			"\"\n",
 			"SET \"TaxonConceptID\" = ", concept_id[1], "\n",
-			"WHERE \"TaxonConceptID\" in (",
+			"WHERE \"TaxonConceptID\" IN (",
 			paste(concept_id[-1], collapse=","), ");\n")
 	dbSendQuery(conn, Query)
 	# change entries as parent
 	Query <- paste0("UPDATE \"", paste0(taxon_relations, collapse = "\".\""),
 			"\"\n",
 			"SET \"Parent\" = ", concept_id[1], "\n",
-			"WHERE \"Parent\" in (", paste(concept_id[-1], collapse=","),");\n")
+			"WHERE \"Parent\" IN (", paste(concept_id[-1], collapse=","),");\n")
 	dbSendQuery(conn, Query)
 	# delete from taxonTraits before deleting concept
 	Query <- paste0("DELETE FROM \"",
 			paste0(taxon_traits, collapse = "\".\""), "\"\n",
-			"WHERE \"TaxonConceptID\" in (",
+			"WHERE \"TaxonConceptID\" IN (",
 			paste(concept_id[-1], collapse=","),");\n")
 	dbSendQuery(conn, Query)
 	# delete old concepts
 	Query <- paste0("DELETE FROM \"",
 			paste0(taxon_relations, collapse = "\".\""), "\"\n",
-			"WHERE \"TaxonConceptID\" in (",
+			"WHERE \"TaxonConceptID\" IN (",
 			paste(concept_id[-1], collapse=","),");\n")
 	dbSendQuery(conn, Query)
 	message("DONE!")
 }
-
-## pg_merge_concepts <- function(conn, schema, concept_id) {
-##     if(length(concept_id) < 2)
-##         stop("Argument 'concept_id' have to be of length 2 or higher.")
-##     # change status of names
-##     query <- paste0("
-## update ", paste0(schema, ".", "names2concepts", collapse=""),"
-## set \"NameStatus\" = 'synonym'
-## where \"TaxonConceptID\" in (", paste(concept_id[-1], collapse=","),");
-## ")
-##     dbSendQuery(conn, query)
-##     # change names to concept
-##     query <- paste0("
-## update ", paste0(schema, ".", "names2concepts", collapse=""), "
-## set \"TaxonConceptID\" = ", concept_id[1],"
-## where \"TaxonConceptID\" in (", paste(concept_id[-1], collapse=","), ");
-## ")
-##     dbSendQuery(conn, query)
-##     # change entries as parent
-##     query <- paste0("
-## update ", paste0(schema, ".", "\"taxonRelations\"", collapse=""), "
-## set \"Parent\" = ", concept_id[1], "
-## where \"Parent\" in (", paste(concept_id[-1], collapse=","),");
-## ")
-##     dbSendQuery(conn, query)
-##     # delete old concepts
-##     query <- paste0("
-## delete from ", paste0(schema, ".", "\"taxonRelations\"", collapse=""), "
-## where \"TaxonConceptID\" in (", paste(concept_id[-1], collapse=","),");
-## ")
-##     dbSendQuery(conn, query)
-##     message("DONE")
-## }
